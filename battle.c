c321250dc986ae07bd85da818d580aacf8695402
aditihaiman <ahaiman00@stuy.edu>
2020-01-13 15:37:44 -0500

    figured out what works with linux

--- battle.c
+++ battle.c
@@ -1,14 +1,18 @@
 #include "players.h"
 
 int main(){
-    //system("chmod 755 battle.sh");
-    printf("A\n");
-    int x;
-    x = execlp("chmod", "chmod", "755", "battle.sh");
-    //int exit_status = system("open -a Terminal \"`pwd`\"");
-    printf("x: %d\n, errno: %d, error: %s", x, errno, strerror(errno));
-    x = execlp("ls", "ls");
-    printf("x: %d\n", x);
+//    printf("A\n");
+//    int x;
+//    x = execlp("chmod", "chmod", "755", "battle.sh");
+//    //int exit_status = system("open -a Terminal \"`pwd`\"");
+//    printf("x: %d\n, errno: %d, error: %s", x, errno, strerror(errno));
+//    x = execlp("ls", "ls");
+//    printf("x: %d\n", x);
 
     //battleMonsters();
+    
+    system("chmod 755 battle.sh");
+    system("./battle.sh");
+    
+    
 }
