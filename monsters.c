0ec084163bb71712d73f432717d1554ebf1b91e0
Sowjanya Sritharasarma <Sowjan95@AC1MTH007HD80.local>
2020-01-15 15:45:23 -0500

    added battleFinalMonsters; fixed small bug with ending

--- monsters.c
+++ monsters.c
@@ -2,11 +2,11 @@
 
 int pipeForBattle(struct monster* monster, struct player* player){
     int fd;
-    
+
     char * myfifo = "/tmp/myfifo2";
-   
+
     mkfifo(myfifo, 0666);
-    
+
     char monsterType[100], playerName[100], pBHealth[1], pHealth[1], pDamage[1], pExp[1], pLev[1];
     strcpy(monsterType, monster->type);
     strcpy(playerName, player->name);
@@ -15,7 +15,7 @@ int pipeForBattle(struct monster* monster, struct player* player){
     sprintf(pDamage, "%d", player->damage);
     sprintf(pExp, "%d", player->experience);
     sprintf(pLev, "%d", player->level);
-    
+
     fd = open(myfifo, O_WRONLY);
     write(fd, monsterType, sizeof(monsterType));
     write(fd, playerName, sizeof(playerName));
@@ -24,7 +24,7 @@ int pipeForBattle(struct monster* monster, struct player* player){
     write(fd, pDamage, sizeof(pDamage));
     write(fd, pExp, sizeof(pExp));
     write(fd, pLev, sizeof(pLev));
-    
+
     close(fd);
     unlink(myfifo);
     return 0;
@@ -32,14 +32,14 @@ int pipeForBattle(struct monster* monster, struct player* player){
 
 int closePipeBattle(struct player* player){ //returns 0 if player won, 1 if player lost
     int fd;
-    
+
     char * myfifo = "/tmp/myfifo";
-    
+
     mkfifo(myfifo, 0666);
-    
+
     char status[1], playerName[100], pBHealth[1], pHealth[1], pDamage[1], pExp[1], pLev[1];
     fd = open(myfifo, O_RDONLY);
-    
+
     read(fd, status, sizeof(status));
     read(fd, playerName, sizeof(playerName));
     read(fd, pBHealth, sizeof(pBHealth));
@@ -47,9 +47,9 @@ int closePipeBattle(struct player* player){ //returns 0 if player won, 1 if play
     read(fd, pDamage, sizeof(pDamage));
     read(fd, pExp, sizeof(pExp));
     read(fd, pLev, sizeof(pLev));
-    
+
     close(fd);
-    
+
     if(strcmp(status, "1")==0) return 1;
     else{
         player->baseHealth = atoi(pBHealth);
@@ -85,7 +85,7 @@ struct monster* randomMonster(int level){
     return MONSTERS[random];
 }
 
-void levelUp(struct player* PLAYER){
+int levelUp(struct player* PLAYER){
   if (PLAYER->level < 3){
       PLAYER->level += 1;
       PLAYER->baseHealth += 10;
@@ -97,8 +97,9 @@ void levelUp(struct player* PLAYER){
   if (PLAYER->level == 3){
       printf("You have reached the last level of the game. You are now ready to go back to your village to deafeat the last few monsters. You are transported back to the village to fulfill your destiny.\n");
       village(PLAYER);
+      return 1;
   }
-
+  return 0;
 }
 
 int battleTroll(struct monster* monster, struct player* player){ //returns 0 if player wins, 1 if player loses
@@ -178,6 +179,7 @@ int battleMonster(struct monster* monster, struct player* player){ //returns 0 i
             printf("Congratulations! You have defeated the %s. You have gained %d points in experience!\n\n", monster->type, monster->damage*2);
             player->experience += monster->damage * 2;
             if ((player->experience >= ((player->level+1)*10)+10) && player->level < 3) levelUp(player);
+            if (player->level == 3) return 1;
             else{
                 int XP_to_health = player->baseHealth - player->health;
                 if (XP_to_health > player->experience) XP_to_health = player->experience;
--- village.c
+++ village.c
@@ -1,5 +1,45 @@
 #include "players.h"
 
+int battleFinalMonsters(struct monster* monster, struct player* player){ //returns 0 if player wins, 1 if player loses
+    char input[100];
+    int output = 0;
+    srand(time(NULL));
+    printf("\nYou have encountered a %s. The %s has %d health and is capable of doing %d damage. You must defeat the %s before you can move on!\n", monster->type, monster->type, monster->health, monster->damage, monster->type);
+    int turn = 0; //0 means it is players turn
+    while (player->health > 0 && monster->health > 0) {
+        if (turn == 0){
+            printf("\nPress \"r\" to roll a dice to determine how much damage you do to the %s. You are capable of doing up to %d damage. ", monster->type, player->damage);
+            if (output!=0) fgetc(stdin);
+            fgets(input, 3, stdin);
+            if (input[0]=='r'){
+                int damage = rand() % player->damage;
+                monster->health -= damage;
+                printf("\nYou have done %d damage. The %s is now at %d health.\n", damage, monster->type, monster->health);
+            }
+        }
+        if (turn == 1) {
+            int damage = rand() % monster->damage;
+            player->health -= damage;
+            printf("\nThe %s has dealt %d damage to you. You are now at %d health.\n", monster->type, damage, player->health);
+        }
+        if (turn == 0) turn = 1;
+        else turn = 0;
+    }
+    if (monster->health <= 0){
+        printf("Congratulations! You have defeated the %s. Your health is restored to %d!\n\n", monster->type, player->baseHealth);
+        player->health += player->baseHealth;
+        //player->health += atoi(input);
+        //printf("Your health is now at %d and your experience is at %d.\n\n", player->health, player->experience);
+        return 0;
+    }
+    if (player->health <= 0){
+        printf("You have been defeated by the %s. Alas, after all your hard work, you could not save your village. Be reborn and try again.\n", monster->type);
+        return 1;
+    }
+    return output;
+}
+
+
 int village(struct player *player){
     printf("\nWelcome back to the village. Four more monsters await you. They will come at you one by one until you have defeated all of them. Whenever you're ready, type 'go' to beging your mission. ");
     char input[100];
@@ -9,14 +49,13 @@ int village(struct player *player){
         fgets(input, 10, stdin);
     }
     int x = 0;
-    x = battleMonster(createMedusa(), player);
-    printf("Done\n");
+    x = battleFinalMonsters(createMedusa(), player);
     if (x == 1) return 1;
-    x = battleMonster(createVoldemort(), player);
+    x = battleFinalMonsters(createVoldemort(), player);
     if (x == 1) return 1;
-    x = battleMonster(createDarthVader(), player);
+    x = battleFinalMonsters(createDarthVader(), player);
     if (x == 1) return 1;
-    x = battleMonster(createPalpatine(), player);
+    x = battleFinalMonsters(createPalpatine(), player);
     if (x == 1) return 1;
     return 0;
 }
